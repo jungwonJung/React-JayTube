145403be2ed930614f0886deecd1e6cb06f00bd1
jungwonJung <wjdwjd1501@gmail.com>
2023-07-26 21:36:34 +0200

    hotfix for deploy

--- src/context/JaytubeApiContext.jsx
+++ src/context/JaytubeApiContext.jsx
@@ -1,11 +1,9 @@
 import { createContext, useContext } from "react";
 import Youtube from "../api/jaytube";
 import YoutubeClient from "../api/jaytubeClient";
-import FakeYoutubeClient from "../api/fakeJaytubeClient";
 
 export const JaytubeApiContext = createContext();
 
-// const client = new FakeYoutubeClient();
 const client = new YoutubeClient();
 const youtube = new Youtube(client);
 export function JaytubeApiProvider({ children }) {
